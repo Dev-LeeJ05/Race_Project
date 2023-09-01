cc72ef4591e2133380e54699afc580c428727525
LeeJoon <leejun9334@gmail.com>
2023-09-01 12:22:50 +0900

    [Add] Player

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,35 +5,45 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     [Header("Use Stat")]
+    public float maxSpeed;
     public float moveSpeed;
     public float rotateSpeed;
     public float traction;
 
     [Space(15f)]
     [Header("Base Stat")]
+    public float baseMaxSpeed;
     public float baseMoveSpeed;
     public float baseRotateSpeed;
     public float baseTraction;
 
     [Space(15f)]
     [Header("Drift Stat")]
+    public float driftMaxSpeed;
     public float driftMoveSpeed;
     public float driftRotateSpeed;
     public float driftTraction;
-    
+    protected bool isDrift;
+
     [Space(15f)]
     [Header("Nitro Stat")]
+    public float nitroMaxSpeed;
     public float nitroMoveSpeed;
     public float nitroRotateSpeed;
     public float nitroTraction;
-    
-    void Start()
+
+    [Space(20f)]
+    [Header("Move Stat")]
+    protected Vector3 moveForce;
+    [SerializeField] protected float dragValue;
+    [SerializeField] protected float brakePower;
+
+    protected virtual void Update()
     {
-        
+        Movement();
     }
 
-    void Update()
-    {
-        
-    }
+    protected virtual void Movement() { }
+
+    protected virtual void Drift() {}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Car
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Update()
     {
-        
+        base.Update();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Movement()
     {
-        
+        base.Movement();
+        moveForce += transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.position += moveForce * Time.deltaTime;
+
+        float z = Input.GetAxisRaw("Horizontal");
+        transform.Rotate(Vector3.forward * z * moveForce.magnitude * rotateSpeed * Time.deltaTime);
+
+        moveForce = Vector3.Lerp(moveForce.normalized, transform.forward, traction * Time.deltaTime) * moveForce.magnitude;
+        moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
+    }
+
+    protected override void Drift()
+    {
+        base.Drift();
+        float driftDir = 0; // 드리프트 방향 저장하는 변수
+
+        // 드리프트 시작 조건
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDrift && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            moveSpeed = driftMoveSpeed;
+            rotateSpeed = driftRotateSpeed;
+            traction = driftTraction;
+            driftDir = Input.GetAxisRaw("Horizontal");
+            isDrift = true;
+        }
+
+        if (isDrift)
+        {
+            // 드리프트 취소 조건
+            if (moveForce.normalized == transform.up || Input.GetAxisRaw("Horizontal") == -driftDir || moveForce.magnitude < driftMoveSpeed)
+            {
+                isDrift = false;
+            }
+
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                traction = driftTraction;
+            }
+            else
+            {
+                traction = baseTraction;
+            }
+        }
+        else
+        {
+            traction = baseTraction;
+            moveSpeed = baseMoveSpeed;
+            rotateSpeed = baseRotateSpeed;
+        }
     }
 }
